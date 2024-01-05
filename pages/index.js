2ab8a5856bf1ebb45c42c5b61e6d98b48b765aee
Pi Pi <diwas118151@gmail.com>
2024-01-05 12:00:37 +0700

    completed header homepage and about page

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ const Home = () => {
         <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0' />
 
         {/* particles */}
-        <div className=''>Particles</div>
+        <ParticlesContainer />
 
         {/* avatar img */}
         <motion.div
