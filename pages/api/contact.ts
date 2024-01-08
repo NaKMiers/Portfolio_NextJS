dfb98018c3653036f3e4059e674cb9549d8ae8e7
Pi Pi <diwas118151@gmail.com>
2024-01-08 15:55:08 +0700

    nothing change

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -20,10 +20,10 @@ function sendMail(to: string, subject: string, html: string) {
 }
 
 export default async function handler(req: any, res: any) {
-  console.log(2131232)
   try {
     if (req.method === 'POST') {
       const { email, firstname, lastname, subject, message } = req.body
+      console.log(req.body)
 
       sendMail(
         process.env.MAIL_TO!,
